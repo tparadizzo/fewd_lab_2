f4ab08dbff89704ebff5ef52362b0dbb5ca4f776
tparadizzo <paradizzotabita@gmail.com>
2020-02-17 21:37:50 +0000

    commit

---
+++ array.js
@@ -0,0 +1,30 @@
+//Question 4
+function myAlphabetLength() {
+    let myAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+    console.log(myAlphabet.length);
+}
+myAlphabetLength();
+
+//Question 5
+let planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter'];
+
+//Question 6
+console.log(planets[0]);
+console.log(planets[1]);
+console.log(planets[2]);
+console.log(planets[3]);
+console.log(planets[4]);
+
+//Question 7
+var a=5;
+var b="hello";
+console.log(a+b);
+
+//Question 11
+var hello = function() {
+    return 'Hello World!';
+}
+var result = hello();
+
+//Question 12
+console.log(hello());
